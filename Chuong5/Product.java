54c668f97fcb10d9f007d933ca2ce07826548717
huybitvvt <huybitvvt@gmail.com>
2026-03-02 01:02:31 +0700

    Add files via upload

---
+++ Chuong5/Product.java
@@ -0,0 +1,30 @@
+package Chuong5;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String name;
+
+    private double price;
+
+    public Product() {}
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+
+    public void setName(String name) { this.name = name; }
+    public void setPrice(double price) { this.price = price; }
+}
